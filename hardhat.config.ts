c12eed9fec480bbfb4d1e3e465aeb3d9c23e8651
IsmailElMoudni <ielmoudn@student.1337.ma>
2021-12-22 23:49:29 +0100

    added rinkeby to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
+// import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 import "@nomiclabs/hardhat-ethers";
@@ -24,12 +24,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.10",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/d6d638e3b25949efafa35d0991b52652",
+      accounts: [process.env.ACCOUNT_PKEY!],
     },
   },
 };
