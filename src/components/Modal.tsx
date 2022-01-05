7509e5e8dac7be645c0679380493260bef37d0ea
IsmailElMoudni <ielmoudn@student.1337.ma>
2022-01-05 10:39:09 +0100

    fix: removed model title

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -31,17 +31,8 @@ const Modal: FunctionComponent<ModalProps> = ({
 
   const modalContent: React.ReactNode | null = show ? (
     <div className="w-screen h-screen z-50 absolute top-0 bg-opacity-70 bg-white/5 backdrop-blur-sm">
-      <div className="w-full h-full flex justify-center items-center p-3lok lg:p-0">
-        <div className="w-full lg:w-1/3 h-3/12 bg-gray-50 relative flex flex-col px-3 py-2 rounded-md mx-2">
-          <div
-            className="absolute top-2 right-2 text-gray-900 lg:font-bold lg:text-2xl rounded-full cursor-pointer mr-3"
-            onClick={(e) => handleCloseClick(e)}
-          >
-            x
-          </div>
-          <div className="text-gray-900 font-bold w-full text-lg lg:text-2xl mb-3 lg:mb-6">
-            {title}
-          </div>
+      <div className="w-full h-full flex justify-center items-center p-3 lg:p-0">
+        <div className="w-full lg:w-1/3 xl:w-1/4 h-3/12 bg-gray-50 relative flex flex-col px-3 py-2 rounded-md mx-2">
           <div className="w-full h-full overflow-hidden flex flex-col">
             {children}
           </div>
