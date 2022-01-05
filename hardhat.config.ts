aadc1b34bbed5f07f6206a7dbaa331e2742612b4
IsmailElMoudni <ielmoudn@student.1337.ma>
2022-01-05 18:14:51 +0100

    fix: fixed hardhat config, undefined values

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,24 @@
-import * as dotenv from "dotenv";
-
+import { config as dotenvConfig } from "dotenv";
 import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 import "@nomiclabs/hardhat-ethers";
+import { resolve } from "path";
 
-dotenv.config();
+dotenvConfig({ path: resolve(__dirname, "./.env.local") });
+
+// Ensure that we have all the environment variables we need.
+const infuraId: string | undefined = process.env.INFURA_ID;
+if (!infuraId) {
+  throw new Error("Please set your Infura Id in a .env file");
+}
+
+const accountPkey: string | undefined = process.env.ACCOUNT_PKEY;
+if (!accountPkey) {
+  throw new Error("Please set your Account Private Key in a .env file");
+}
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -18,11 +29,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 });
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.10",
+  solidity: {
+    version: "0.8.10",
+  },
   networks: {
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/d6d638e3b25949efafa35d0991b52652",
-      accounts: [""],
+      url: `https://rinkeby.infura.io/v3/${infuraId}`,
+      accounts: [accountPkey],
     },
   },
 };
