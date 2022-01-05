26729006a8365addb5e2edcccb653f9840cb5c7e
IsmailElMoudni <ielmoudn@student.1337.ma>
2022-01-05 11:03:44 +0100

    fix: Minor fixes

--- src/components/BalanceBox.tsx
+++ src/components/BalanceBox.tsx
@@ -1,6 +1,7 @@
 import { FunctionComponent } from "react";
 import FeatherIcon from "feather-icons-react";
 import Countdown from "react-countdown";
+import useSave from "../Hooks/useSave";
 
 interface IProps {
   balance: string;
@@ -13,8 +14,9 @@ const BalanceBox: FunctionComponent<IProps> = ({
   endTime,
   setIsModalOpen,
 }) => {
+  const { withdrawAll } = useSave();
   return (
-    <div className="w-full md:w-80 rounded-md h-32 px-5 py-3 flex flex-col justify-between items-center bg-gray-900 text-gray-100">
+    <div className="w-full md:w-80 rounded-md h-36 px-5 py-3 flex flex-col justify-between items-center bg-gray-900 text-gray-100">
       <div className="w-full items-center">
         <div className="flex justify-between w-full">
           <div className="text-md">Ethereum</div>
@@ -24,13 +26,16 @@ const BalanceBox: FunctionComponent<IProps> = ({
         </div>
         <div className="text-2xl text-center">{balance} ETH</div>
       </div>
-      {endTime ? (
+      {endTime && Date.now() - endTime < 0 ? (
         <div className="w-full flex justify-between">
           <div>Remaining:</div>
-          <Countdown date={new Date(endTime * 1000)}></Countdown>
-          {/* <pre>{JSON.stringify(saving.endTime, null, 2)}</pre> */}
+          <Countdown date={new Date(endTime)}></Countdown>
         </div>
-      ) : null}
+      ) : (
+        Number(balance) !== 0 && (
+          <button onClick={() => withdrawAll()}>Withdraw</button>
+        )
+      )}
     </div>
   );
 };
--- src/pages/savings.tsx
+++ src/pages/savings.tsx
@@ -22,23 +22,20 @@ const Savings: FunctionComponent = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { fetchBalance } = useSave();
   const [{ balance, endTime }, setSaving] = useState<ISaving>(initialSaving);
+  const [lastTx, setLastTx] = useState<string>("");
 
   useEffect(() => {
     if (address) {
       (async () => {
         const _balance = await fetchBalance(address);
+        console.log("balance", _balance);
         setSaving({
           balance: ethers.utils.formatEther(_balance.balance.toString()),
           endTime: _balance.endTime.toNumber(),
         });
-
-        console.log({
-          balance: _balance.balance.toString(),
-          date: _balance.endTime.toString(),
-        });
       })();
     }
-  }, [address]);
+  }, [address, lastTx]);
 
   if (!web3Provider || address === null) return <UserNotConnected />;
 
@@ -49,7 +46,7 @@ const Savings: FunctionComponent = () => {
         onClose={setIsModalOpen}
         title="Add Balance to your savings"
       >
-        <SavingForm />
+        <SavingForm setIsModalOpen={setIsModalOpen} setLastTx={setLastTx} />
       </Modal>
       <div className="container w-full md:w-2/3 lg:w-1/2 md:mx-8 lg:mx-20">
         <div className="flex flex-wrap">
