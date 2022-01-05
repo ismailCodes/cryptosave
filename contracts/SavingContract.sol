22ca89e92cf126caf250ecb904b94b39ea1d7da2
IsmailElMoudni <ielmoudn@student.1337.ma>
2022-01-05 10:38:39 +0100

    fix: user can only withdraw all

--- contracts/SavingContract.sol
+++ contracts/SavingContract.sol
@@ -8,7 +8,7 @@ contract MoneySaver {
     }
     address public owner;
     mapping(address => Saving) public balances;
-    uint256 daySeconds = 86400;
+    uint256 internal daySeconds = 86400;
 
     //requires the user to send a positive amount of ETH
     modifier onlyPositive(uint256 _amount) {
@@ -25,15 +25,7 @@ contract MoneySaver {
     modifier onlyValidTimeWithdraw() {
         require(
             block.timestamp > balances[msg.sender].endTime,
-            "You cannot withdraw yet"
-        );
-        _;
-    }
-
-    modifier balanceOrLess(uint256 _amount) {
-        require(
-            balances[msg.sender].balance >= _amount,
-            "Withdraw your balance or less"
+            "Wait for withdrawal time"
         );
         _;
     }
@@ -51,17 +43,10 @@ contract MoneySaver {
         }
     }
 
-    function withdraw(uint256 _amount)
-        public
-        onlyPositive(_amount)
-        onlyValidTimeWithdraw
-        balanceOrLess(_amount)
-    {
-        balances[msg.sender].balance -= _amount;
-        //if amount is equal to balance, then endTime is set to 0
-        if (balances[msg.sender].balance == 0) {
-            balances[msg.sender].endTime = 0;
-        }
-        payable(msg.sender).transfer(_amount);
+    function withdrawAll() public onlyValidTimeWithdraw {
+        uint256 balance = balances[msg.sender].balance;
+        balances[msg.sender].balance -= balance;
+        balances[msg.sender].endTime = 0;
+        payable(msg.sender).transfer(balance);
     }
 }
