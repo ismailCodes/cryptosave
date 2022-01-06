af2b1c0332d3b827bea9cc3b9af1e150262da986
IsmailElMoudni <ielmoudn@student.1337.ma>
2022-01-06 12:16:00 +0100

    Added Modal shadow

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -26,7 +26,7 @@ const Modal: FunctionComponent<ModalProps> = ({ show, onClose, children }) => {
   const modalContent: React.ReactNode | null = show ? (
     <div className="w-screen h-screen z-50 absolute top-0 bg-opacity-70 bg-white/5 backdrop-blur-sm">
       <div className="w-full h-full flex justify-center items-center p-3 lg:p-0">
-        <div className="w-full lg:w-1/3 xl:w-1/4 h-3/12 bg-gray-50 relative flex flex-col px-3 py-2 rounded-md mx-2">
+        <div className="w-full lg:w-1/3 xl:w-1/4 h-3/12 bg-gray-50 relative flex flex-col px-3 py-2 rounded-md mx-2 shadow-xl shadow-gray-700/40">
           <div className="w-full h-full overflow-hidden flex flex-col">
             {children}
           </div>
