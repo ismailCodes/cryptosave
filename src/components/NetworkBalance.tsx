75eaa782524fa0e22721cffc148929201c89a585
IsmailElMoudni <ielmoudn@student.1337.ma>
2022-01-06 13:40:53 +0100

    UI minor fixes

--- src/components/NetworkBalance.tsx
+++ src/components/NetworkBalance.tsx
@@ -6,9 +6,9 @@ const NetworkBalance: FunctionComponent = () => {
   return (
     <div className="flex flex-col text-center justify-center items-center my-3 lg:my-0 text-sm lg:mr-2">
       <div className="text-gray-50 pb-1 lg:pb-0">
-        {balance} {chainData.native_currency.symbol}
+        {balance} {chainData?.native_currency.symbol}
       </div>
-      <div className="text-gray-50">{chainData.name}</div>
+      <div className="text-gray-50">{chainData?.name}</div>
     </div>
   );
 };
